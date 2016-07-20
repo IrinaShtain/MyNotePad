812b7de0168bafa919673275f0cb5d4321bc9d2c
Irina Shtain <shtain_irina@ukr.net>
2016-07-20 20:47:00 +0300

    Creating Toolbar, ViewPager, TabLayout with Material Design style

---
+++ app/src/main/java/shtainyky/com/mynotepad/Adapter/TabPagerFragmentAdapter.java
@@ -0,0 +1,43 @@
+package shtainyky.com.mynotepad.Adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import shtainyky.com.mynotepad.Fragments.FragmentForNew;
+import shtainyky.com.mynotepad.Fragments.FragmentListForCreated;
+import shtainyky.com.mynotepad.R;
+
+public class TabPagerFragmentAdapter extends FragmentPagerAdapter {
+   private String[] tabs;
+    public TabPagerFragmentAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        tabs = context.getResources().getStringArray(R.array.tabs_array);
+
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0:
+                return new FragmentForNew();
+            case 1:
+                return new FragmentListForCreated();
+            case 2:
+                return new FragmentForNew();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
---
+++ app/src/main/java/shtainyky/com/mynotepad/Fragments/FragmentForNew.java
@@ -0,0 +1,21 @@
+package shtainyky.com.mynotepad.Fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import shtainyky.com.mynotepad.R;
+
+public class FragmentForNew extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_new, container,false);
+        return view;
+    }
+}
