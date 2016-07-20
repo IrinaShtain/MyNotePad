dd041e7b7f41dc23efba5a2bc5f7ebf1f2f53020
Irina Shtain <shtain_irina@ukr.net>
2016-07-20 19:43:35 +0300

    Creating Toolbar, TabLayout, ViewPager in style Material Design.

--- app/src/main/java/shtainyky/com/mynotepad/MainActivity.java
+++ app/src/main/java/shtainyky/com/mynotepad/MainActivity.java
@@ -1,13 +1,49 @@
 package shtainyky.com.mynotepad;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import shtainyky.com.mynotepad.Adapter.TabPagerFragmentAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        initTabs();
+
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        TabPagerFragmentAdapter adapter = new TabPagerFragmentAdapter(this,getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.mipmap.ic_calendar_plus);
+        tabLayout.getTabAt(1).setIcon(R.mipmap.ic_calendar_check);
+        tabLayout.getTabAt(2).setIcon(R.mipmap.ic_crown);
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
